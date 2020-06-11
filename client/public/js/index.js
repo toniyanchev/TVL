a59a1dfb994ef20c40047355d2296c74d299a5fa
Victor <victor@gorchilov.com>
2020-06-11 15:29:37 +0300

    up: create cards for main page

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -11,15 +11,14 @@ con.connect()
 
 con.query("SELECT * FROM games", function (err, rows, fields) {
     if (err) throw err
-
     console.log("The solution is: ", rows[0].solution)
 })
 
-module.export = function getUser(id) {
+module.export = async function getUser(id) {
     return con.query(`SELECT * FROM users WHERE id=${id}`)
 }
 
-module.export = function getGames(qry) {
+module.export = async function getGames(qry) {
     if (qry == undefined) {
         return con.query(`SELECT * FROM games`)
     }
