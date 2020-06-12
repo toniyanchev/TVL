1182f46c23eeab1e7a27dcde60736f03d70c8262
Victor <victor@gorchilov.com>
2020-06-12 09:57:04 +0300

    rm: mysql

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-
-console.log("Hi");
+console.log("Hi")
 const pg = require("pg")
 
-let connection = "postgres://userName:password@localhost/ip:3306/GameShop"
+// let connection = "postgres://userName:password@localhost/ip:3306/GameShop"
 
-let client = new pg.Client(connection)
+// let client = new pg.Client(connection)
 
-client.connect()
-
-exports.getGames = async (category, name) => {
-    client
-        .query(`SELECT * FROM games WHERE name = '${query}'`)
-        .then((res) => {
-            return res
-        })
-        .catch((e) => console.error(e.stack))
-}
+// client.connect()
